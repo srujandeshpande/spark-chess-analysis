fb1dff54595757dce385a2911ed640f18388f0ae
Srujan Deshpande <srujan.deshpande@gmail.com>
2020-12-04 12:55:13 +0530

    Captures formatted

--- capture.py
+++ capture.py
@@ -1,44 +1,49 @@
-# Program for finding upset rate 
+# Finding if more captures results in a win
 
+# Imports
 import sys
-
 from pyspark import SparkContext, SparkConf
-from pyspark.sql import SparkSession
 
+# Creating a spark context
+sc = SparkContext("local", "More captures to win")
 
-sc = SparkContext("local","Finding upset rate")
-
+# Reading the dataset from HDFS
 text_file = sc.textFile("hdfs://chess3-m/user/hdfs/data.txt")
 
-table2 = text_file.filter(lambda x: "#" not in x.split(' ')[0])
-table3 = table2.filter(lambda x: "blen_false" in x.split(' ')[15])
-table4 = table3.filter(lambda x: len(x.split(' ')[2]) == 3)
+# Filtering to remove comments from the dataset
+table2 = text_file.filter(lambda x: "#" not in x.split(" ")[0])
+# Filtering to remove games with no specified length
+table3 = table2.filter(lambda x: "blen_false" in x.split(" ")[15])
+# Filtering to remove games whos result was a draw
+table4 = table3.filter(lambda x: len(x.split(" ")[2]) == 3)
+
 
 def game_map(lines):
+    # Split each line at space
     lineArr = lines.split(" ")
+    # Initialize number of captures to 0
     w = 0
     b = 0
+    # For each move, check if there was a capture. If captured, increment the player capture count
     for i in lineArr[17:]:
-        if 'x' in i:
-            if 'W' in i:
-                w+=1
+        if "x" in i:
+            if "W" in i:
+                w += 1
             else:
-                b+=1
-
-    if(lineArr[2] == "1-0" and w>b):
+                b += 1
+    # Return output based on win or lose compared to number of captures
+    if lineArr[2] == "1-0" and w > b:
         return ("More captures", 1)
-    elif(lineArr[2] == "0-1" and b>w):
+    elif lineArr[2] == "0-1" and b > w:
         return ("More captures", 1)
     else:
         return ("Less captures", 1)
 
-    return (num_moves, 1)
 
+# Reducer sums the count by more or lesscaptures
+outRDD = table4.map(game_map).reduceByKey(lambda a, b: a + b)
 
-outRDD = table4.map(game_map).reduceByKey(lambda a,b: a+b)
-
-#outRDD.saveAsTextFile("hdfs://chess3-m/user/hdfs/out")
-
+# Convert RDD to list and print
 j = list(outRDD.take(2))
 print(j)
 
--- upsetrate.py
+++ upsetrate.py
@@ -1,5 +1,5 @@
-# Finding the Upset rate for different intervals 
-#A game is upset when the player with a lower Elo wins against a player with a higher Elo
+# Finding the Upset rate for different intervals
+# A game is upset when the player with a lower Elo wins against a player with a higher Elo
 
 # Imports
 import sys
@@ -11,40 +11,42 @@ sc = SparkContext("local", "Calculating upset rate")
 # Reading the dataset from HDFS
 text_file = sc.textFile("hdfs://chess2-m/user/hdfs/data.txt")
 
-#Computing the rate for different Elo intervals
+# Computing the rate for different Elo intervals
 for gap in range(100, 500, 50):
 
     # Filtering to remove comments from the dataset
-    table2 = text_file.filter(lambda x: "#" not in x.split(' ')[0])
+    table2 = text_file.filter(lambda x: "#" not in x.split(" ")[0])
     # Filtering to remove games where the White player's Elo is not given
-    table3 = table2.filter(lambda x: "welo_false" in x.split(' ')[8])
+    table3 = table2.filter(lambda x: "welo_false" in x.split(" ")[8])
     # Filtering to remove games where the Black player's Elo is not given
-    table4 = table3.filter(lambda x: "belo_false" in x.split(' ')[9])
+    table4 = table3.filter(lambda x: "belo_false" in x.split(" ")[9])
     # Filtering to remove games whos result was a draw
-    table5 = table4.filter(lambda x: len(x.split(' ')[2]) == 3)
-    #Filtering to find games where the Elo difference is within the specified range
-    table6 = table5.filter(lambda x: (gap <= abs(int(x.split(' ')[3]) - int(x.split(' ')[4])) < gap+50))
+    table5 = table4.filter(lambda x: len(x.split(" ")[2]) == 3)
+    # Filtering to find games where the Elo difference is within the specified range
+    table6 = table5.filter(
+        lambda x: (gap <= abs(int(x.split(" ")[3]) - int(x.split(" ")[4])) < gap + 50)
+    )
 
     def game_map(lines):
-        #Split each line at space
+        # Split each line at space
         lineArr = lines.split(" ")
-        #Checking if White has won and Upset occured
-        if(lineArr[2] == '1-0' and int(lineArr[3])<int(lineArr[4])):
+        # Checking if White has won and Upset occured
+        if lineArr[2] == "1-0" and int(lineArr[3]) < int(lineArr[4]):
             return ("Upset", 1)
         # Checking if Black has won and Upset ocurred
-        elif(lineArr[2] == '0-1' and int(lineArr[3])>int(lineArr[4])):
+        elif lineArr[2] == "0-1" and int(lineArr[3]) > int(lineArr[4]):
             return ("Upset", 1)
         else:
             return ("Not Upset", 1)
 
     # Reducer sums the count for upset and not upset
-    outRDD = table6.map(game_map).reduceByKey(lambda a,b: a+b)
+    outRDD = table6.map(game_map).reduceByKey(lambda a, b: a + b)
 
     # Extracting the two values as a list
     j = list(outRDD.take(2))
 
-    #Calculating and printing the upset percentage
-    p = (int(j[0][1])/int(j[1][1]))*100
-    print(f'Upset Rate for Range {gap} to {gap+50} is: {p}%')
+    # Calculating and printing the upset percentage
+    p = (int(j[0][1]) / int(j[1][1])) * 100
+    print(f"Upset Rate for Range {gap} to {gap+50} is: {p}%")
 
 sc.stop()
